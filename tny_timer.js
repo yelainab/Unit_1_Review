524bdc01a4deb71389ca63454c8c32ab71c5385a
Yelaina <ybuford201@west-mec.org>
2020-01-10 17:25:55 -0700

    step 7

--- tny_timer.js
+++ tny_timer.js
@@ -10,11 +10,26 @@
    Date:   1/9/20
 
 */
-function showClock(){
-   var thisDate = new Date("May 19, 2018");
-   //var localDate = thisDate
-   //var localTime = 
+showClock();
+setInterval("showClock()", 1000);
 
+function showClock(){
+   var thisDay = new Date(2018, 4, 19, 9, 31, 27);
+   var localDate = thisDay.toLocaleDateString();
+   var localTime = thisDay.toLocaleTimeString();
+   document.getElementById("currentTime").innerHTML="<span>" + localDate + "</span><span>" + localTime + "<span>";
+   var j4Date = nextJuly4(thisDay);
+   
+   j4Date.setHours(21, 0, 0, 0);
+   var days = (j4Date - thisDay)/(1000*60*60*24);
+   var hrs = (days - Math.floor(days))*24;
+   var mins = (hrs - Math.floor(hrs))*60;
+   var secs = (mins - Math.floor(mins))*60;
+
+   document.getElementById("dLeft").innerHTML= Math.floor(days);
+   document.getElementById("hLeft").innerHTML= Math.floor(hrs);
+   document.getElementById("mLeft").innerHTML= Math.floor(mins);
+   document.getElementById("sLeft").innerHTML= Math.ceil(secs);
 }
 
 function nextJuly4(currentDate) {
@@ -24,7 +39,7 @@ function nextJuly4(currentDate) {
    if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
    return jDate;
 }
-   var days = (newYear - currentYear)/(1000*60*60*24);
-   var hrs = (daysLeft - Math.floor(daysLeft))*24;
-   var mins = (hrsLeft - Math.floor(hrsLeft))*60;
-   var secs = (minsLeft - Math.floor(minsLeft))*60
+
+
+
+///setInterval("showClock()", 1000);
